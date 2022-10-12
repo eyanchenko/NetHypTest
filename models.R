43d92dff6f9efd32b91f6e13b1d8b3b7dfc20f67
eyanchenko <37027295+eyanchenko@users.noreply.github.com>
2022-10-12 09:20:28 -0400

    Update models.R

--- models.R
+++ models.R
@@ -1,52 +1,54 @@
-generateER <- function(n, p){
+generateDCBM <- function(theta, p11, p12, p22, prop=0.50){
+  #theta: different degrees
+  #pin: prob. of connection in comm.
+  #pout: prob. of connectionb between comm.
   
-  A <- matrix(0, nrow = n, ncol = n)
+  n <- length(theta)
+  m <- as.integer(round(n*prop, digits=0))
   
-  A[col(A) > row(A)] <- rbinom(n*(n-1)/2, 1, p)
+  Omega11 <- matrix(p11,  ncol = m,   nrow = m)
+  Omega12 <- matrix(p12, ncol = n-m, nrow = m)
+  Omega22 <- matrix(p22,  ncol = n-m, nrow = n-m) 
   
-  A = A+t(A)
+  Omega <- rbind(cbind(Omega11, Omega12), cbind(t(Omega12), Omega22) )
   
-  return(A)
-  
-}
-
-generateCLa <- function(A){
-  
-  n = dim(A)[1]
-  m = sum(A)/2
-  d = rowSums(A)
-  
-  #NOTE THAT IF A HAS LOTS OF EDGES, THEN SOME ENTRIES >1
-  P.hat  = as.vector(d %*% base::t(d) / (2*m))
-  P.hat[P.hat>1] <- 0.999
-  
-  CL <- matrix(rbinom(n^2, 1, P.hat), ncol=n, nrow=n)
-  
-  CL[lower.tri(CL, diag=TRUE)] <- 0
-  
-  CL = CL + t(CL)
-  
-  return(CL)
-}
-
-
-generateLSM <- function(n, d, beta, sigma2=1){
-  
-  expit <- function(x){1/(1+exp(-x))}
-  expit <- Vectorize(expit)
-  
-  require(mvtnorm)
-  
-  #Latent space position
-  z <- rmvnorm(n, mean=rep(0,d), sigma=sigma2 * diag(d))
-  
-  P = as.vector(expit(beta - as.matrix(dist(z, method="euclidean"))))
+  P = as.vector(theta %*% t(theta) * Omega)
   
   A <- matrix(rbinom(n^2, 1, P), ncol=n, nrow=n)
   
   A[lower.tri(A, diag=TRUE)] <- 0
-  A = A + t(A)
+  A = A + base::t(A)
+  
+  return(A) 
+  
+}
+
+
+generateDCBMK <- function(theta, pin, pout, props){
+  
+  n <- length(theta)
+  K <- length(props)
+  
+  Omega <- matrix(0, nrow=n, ncol=n)
+  
+  idx <- 1
+  # Block probabilities
+  for(k in 1:K){
+    intra <- idx:(idx + n*props[k]-1)
+    inter <- (1:n)[-intra]
+    Omega[intra, intra] <- pin  # intra-community probs.
+    Omega[intra, inter] <- pout # inter-community probs.
+    Omega[inter, intra] <- pout
+    
+    idx =  idx + n*props[k]
+  }
+  
+  P = as.vector(theta %*% t(theta) * Omega)
+  
+  A <- matrix(rbinom(n^2, 1, P), ncol=n, nrow=n)
+  
+  A[lower.tri(A, diag=TRUE)] <- 0
+  A = A + base::t(A)
   
   return(A)
-  
 }
